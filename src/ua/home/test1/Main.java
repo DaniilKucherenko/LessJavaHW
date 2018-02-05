8d6e05f47710038673ecdca16744995dcb4a5d7c
Daniil <Daniil.ukraine@gmail.com>
2018-02-05 19:24:23 +0200

    first commit

---
+++ src/ua/home/test1/Main.java
@@ -0,0 +1,25 @@
+package ua.home.test1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Введите число a");
+		System.out.println();
+		int a=sc.nextInt();
+		
+		int b =a;
+		
+		if(a%b==0||a%1==0) {
+			System.out.println("Число a простое");
+			
+			
+		}else {
+			System.out.println("Число a сложенное");
+		}
+
+	}
+
+}
