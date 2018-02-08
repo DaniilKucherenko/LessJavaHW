3a522553d9feb8669ec8f2b170b04599ff69f997
Daniil <Daniil.ukraine@gmail.com>
2018-02-08 21:50:49 +0200

    tst

--- src/ua/home/test1/Main.java
+++ src/ua/home/test1/Main.java
@@ -5,26 +5,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Введите число a");
 		System.out.println();
-		
 
-		
-		int a=sc.nextInt();
+		int a = sc.nextInt();
 
-		
-	for(int i=2; i<=Math.sqrt(a); i++) {
-		
-		if(a%i==0) {
-			System.out.println("Число  простое");
-			
-		
-		}else {
-			System.out.println("Число  сложенное");
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0) {
+				System.out.println("Число " + a + " сложенное");
+				return;
+			}
 		}
-
+		System.out.println("Число " + a + " простое");
 	}
 
-}
 }
